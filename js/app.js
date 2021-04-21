9e2aca731695da25ce55aba9ae20f16b96ea20a0
Ben.con <041ymoob@gmail.com>
2021-04-21 11:11:52 -0700

    finished

--- js/app.js
+++ js/app.js
@@ -1,41 +1,5 @@
 
 
-baguetteBox.run('.gallery', {
- const: buttons = Boolean | 'auto'
-});
+baguetteBox.run('.gal');
 const search = new Filter('search', 'data-caption');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const list = document.querySelectorAll(".gal figcaption")
-// const bar = document.forms["search"].querySelector('input');
-// console.log (list)
-
-// console.log(bar)
-// bar.addEventListener ("keyup",function(e) {
-//     const term = e.target.value.toLowerCase();
-//     const photos = list.getElementsByTagName('figcaption');
-//     Array.from(photos).forEach(function(photo){
-//         const title = photo.firstElementChild.textContent;
-//         if(title.toLowerCase().indexOf(term) != -1){
-//             photo.style.display = "grid"
-//         }else{
-//             photo.style.display = "none"
-
-//         }
-//     })
-//     console.log(e.target.value)
-// })
